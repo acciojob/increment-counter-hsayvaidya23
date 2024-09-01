c9fce8bda26feca366faf1198fc32013ca0a4942
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-01 14:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let counter = document.getElementById('counter');
+let inc = document.getElementById('incrementBtn')
+
+let counterValue = 0;
+
+inc.addEventListener('click', () => {
+	alert(counterValue);
+	counterValue++;
+	counter.textContent = counterValue;
+})
